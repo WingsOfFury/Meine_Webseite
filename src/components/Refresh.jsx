196b6a546219d7c358a26b3228bcea7f6e6f11ce
WingsOfFury <akcengiz@gmx.de>
2024-05-20 23:57:36 +0200

    Badges eingefuegt

--- src/components/Refresh.jsx
+++ src/components/Refresh.jsx
@@ -35,7 +35,7 @@ const Refresh = () => {
         // Intervall alle 3 Sekunden für das Aktualisieren der API-Daten
         const intervalId = setInterval(() => {
             fetchUserData();
-        }, 40000)
+        }, 5000)
 
         // Aufräumen: Interval stoppen, wenn die Komponente unmontiert wird
         return () => clearInterval(intervalId);
