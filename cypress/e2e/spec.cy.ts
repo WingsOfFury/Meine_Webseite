998d7d06f25fcb7112e9bdf8957d3cd2fd9901b7
WingsOfFury <akcengiz@gmx.de>
2024-06-08 00:40:50 +0200

    Cypress Blog Video eingefuegt

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,24 +1,24 @@
 describe('home page', () => {
-  it('the buttons are there', () => {
+  it('ist about dabei', () => {
     cy.visit('http://192.168.0.199:4321/')
     
+    
+    
     cy.viewport('macbook-15')
+    cy.contains("About").should("exist")
+    cy.contains("Blog").should("exist")
+    cy.contains("CV").should("exist")
+    cy.contains("Testimonials").should("exist")
+    cy.contains("Akansel Cengiz").should("exist")
+
 
     cy.get("Header").should("exist")
-    cy.get("#themeToggle").click()
+    cy.get("#themeToggle").should("exist").click().end()
+    cy.get(".inline-flex").click()
 
 
-    // cy.get(".cursor-pointer").should("exist").click()
-    // .contains("Blog").click()
+  
 
-    // cy.get(".cursor-pointer").should("exist").click()
-    // .contains("Contact")
-
-    // cy.get(".cursor-pointer").should("exist").click()
-    // .contains("About")
-
-    // cy.get(".cursor-pointer").should("exist").click()
-    // .contains("Testimonial").should("exist").click()
 
     
     
